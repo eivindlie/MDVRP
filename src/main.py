b9ce2591035bc967311e30f9a6916580b7f6873e
Eivind Lie Andreassen <ela4991@gmail.com>
2019-02-08 12:43:48 +0100

    Switch to indirect route encoding

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from data_classes import Depot, Customer
 from utility import distance
 
 
-population_size = 20
+population_size = 100
 
 depots = None
 customers = None
@@ -58,7 +58,7 @@ def evaluate(chromosome):
     score = 0
     for depot_index in range(len(chromosome)):
         depot = depots[depot_index]
-        for route in chromosome[depot_index]:
+        for route in routify(chromosome[depot_index], depot):
             customer = None
             last_pos = depot.pos
             for cid in route:
@@ -69,6 +69,33 @@ def evaluate(chromosome):
     return score
 
 
+def routify(cust, depot):
+    d = []
+    route = []
+    route_load = 0
+    route_distance = 0
+    last_pos = depot.pos
+    for cid in cust:
+        customer = customers[cid - 1]
+        _, return_distance = find_closest_depot(customer.pos)
+        travel_distance = distance(last_pos, customer.pos)
+        if route_load + customer.demand < depot.max_load \
+                and (depot.max_duration == 0
+                     or travel_distance + customer.service_duration + return_distance < depot.max_duration):
+            route_distance += travel_distance + customer.service_duration
+            route_load += customer.demand
+            route.append(customer.id)
+        else:
+            # assert len(d) < depot.max_vehicles, 'No more vehicles available - ' \
+            #                                    'too many customers assigned to depot'
+            d.append(route)
+            route_distance = distance(customer.pos, depot.pos) + customer.service_duration
+            route_load = customer.demand
+            route = [customer.id]
+        last_pos = customer.pos
+    return d
+
+
 def create_initial_population():
     global population
     population = []
@@ -76,31 +103,9 @@ def create_initial_population():
     for i in range(population_size):
         chromosome = []
         for depot in depots:
-            d = []
             shuffle(depot.closest_customers)
-
-            route = []
-            route_load = 0
-            route_distance = 0
-            last_pos = depot.pos
-            for customer in depot.closest_customers:
-                _, return_distance = find_closest_depot(customer.pos)
-                travel_distance = distance(last_pos, customer.pos)
-                if route_load + customer.demand < depot.max_load \
-                    and (depot.max_duration == 0
-                         or travel_distance + customer.service_duration + return_distance < depot.max_duration):
-                    route_distance += travel_distance + customer.service_duration
-                    route_load += customer.demand
-                    route.append(customer.id)
-                else:
-                    assert len(d) < depot.max_vehicles, 'No more vehicles available - ' \
-                                                        'too many customers assigned to depot'
-                    d.append(route)
-                    route_distance = distance(customer.pos, depot.pos) + customer.service_duration
-                    route_load = customer.demand
-                    route = [customer.id]
-                last_pos = customer.pos
-
+            d = list(map(lambda x: x.id, depot.closest_customers[:]))
+            # d = routify(depot.closest_customers, depot)
             chromosome.append(d)
         population.append(chromosome)
 
@@ -118,8 +123,7 @@ def reproduce(elitism=2):
         parents = []
         for j in range(2):
             # Use an exponential distribution, which is more likely to draw the best chromosomes
-            p = int(expovariate(1/((population_size / 2) - 2)))
-            p = min(p, population_size - 1)
+            p = int(expovariate(1/((population_size / 2) - 2))) % population_size
             parents.append(population[p])
 
         # TODO Implement crossover
@@ -127,7 +131,7 @@ def reproduce(elitism=2):
     population = new_population
 
 
-def mutate(rate=0.1):
+def mutate(rate=0.01):
     for chromosome in population:
         if random() > rate:
             continue
@@ -135,12 +139,15 @@ def mutate(rate=0.1):
         sel = random()
 
         if sel < 0.3:
-            # Reverse a single route
-            depot = int(random() * len(chromosome))
-            if not len(chromosome[depot]):
+            # Reverse a cut from the chromosome
+            depot_index = int(random() * len(chromosome))
+            depot = chromosome[depot_index]
+            if not len(depot):
                 continue
-            route = int(random() * len(chromosome[depot]))
-            chromosome[depot][route].reverse()
+            cut1 = int(random() * len(depot))
+            cut2 = cut1 + int(random() * (len(depot) - cut1))
+
+            chromosome[depot_index] = depot[:cut1] + depot[cut2-1:cut1-1:-1] + depot[cut2:]
 
 
 def train(generations):
@@ -154,7 +161,7 @@ def train(generations):
 
 
 if __name__ == '__main__':
-    load_problem('../data/p02')
+    load_problem('../data/p06')
     cluster()
     create_initial_population()
 
